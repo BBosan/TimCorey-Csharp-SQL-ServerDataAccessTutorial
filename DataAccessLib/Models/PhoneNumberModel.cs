914391ae9224dfcd1b7e2de4248ab84cbd4d8fb6
BBosna <maracparac@gmail.com>
2021-11-15 18:09:30 +0100

    Added PhoneNumber Class to Models Folder

---
+++ DataAccessLib/Models/PhoneNumberModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLib.Models
+{
+    public class PhoneNumberModel
+    {
+        public int Id { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
