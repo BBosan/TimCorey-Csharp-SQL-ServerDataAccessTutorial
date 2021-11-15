8136e8a4da58044bff5fdadfd386f0189a6e6e1d
BBosna <maracparac@gmail.com>
2021-11-15 18:30:23 +0100

    Added GetFullContactById Method to SqlCrud Class

--- DataAccessLib/SqlCrud.cs
+++ DataAccessLib/SqlCrud.cs
@@ -1,6 +1,7 @@
 ﻿using DataAccessLib.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace DataAccessLib
@@ -19,6 +20,39 @@ namespace DataAccessLib
         {
             string sql = "select Id, FirstName, LastName from dbo.Contacts";
             return db.LoadData<BasicContactModel, dynamic>(sql, new { }, _connectionString);
+        }
+
+        public FullContactModel GetFullContactById(string id)
+        {
+            string sql = "select Id, FirstName, LastName from dbo.Contacts where Id = @Id";
+
+            FullContactModel output = new FullContactModel();
+
+            output.BasicInfo = db.LoadData<BasicContactModel, dynamic>(sql, new { Id = id }, _connectionString).FirstOrDefault();
+
+            if (output.BasicInfo == null)
+            {
+                //do something to tell the user that the record was not found
+                //throw new Exception("User not found");
+                return null;
+            }
+
+            sql = @"select e.*
+                    from dbo.EmailAddresses e
+                    inner join dbo.ContactEmailTable ce on ce.EmailAddressId = e.Id
+                    where ce.ContactId = @Id";
+
+            output.EmailAddresses = db.LoadData<EmailAddressModel, dynamic>(sql, new { Id = id }, _connectionString);
+
+            sql = @"select p.*
+                    from dbo.PhoneNumbers p
+                    inner join dbo.ContactPhoneNumbers cp on cp.PhoneNumberId = p.Id
+                    where cp.ContactId = @Id";
+
+            output.PhoneNumbers = db.LoadData<PhoneNumberModel, dynamic>(sql, new { Id = id }, _connectionString);
+
+
+            return output;
         }
 
     }
