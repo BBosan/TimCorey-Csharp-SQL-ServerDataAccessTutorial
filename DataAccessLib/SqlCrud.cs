a719c49504d22e917f4eaaf5bd8620a0c292c3b7
BBosna <maracparac@gmail.com>
2021-11-15 18:01:27 +0100

    Added GetAllContacts Method to SqlCrud

--- DataAccessLib/SqlCrud.cs
+++ DataAccessLib/SqlCrud.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataAccessLib.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,9 +8,17 @@ namespace DataAccessLib
     public class SqlCrud
     {
         private readonly string _connectionString;
+        private SqlDataAccess db = new SqlDataAccess();
+
         public SqlCrud(string connectionString)
         {
             _connectionString = connectionString;
+        }
+
+        public List<BasicContactModel> GetAllContacts()
+        {
+            string sql = "select Id, FirstName, LastName from dbo.Contacts";
+            return db.LoadData<BasicContactModel, dynamic>(sql, new { }, _connectionString);
         }
 
     }
